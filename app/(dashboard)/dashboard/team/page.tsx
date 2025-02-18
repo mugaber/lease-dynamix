6ee30d7e2c2f88f88e01d608325a2f595105cbc8
mugaber <mohammedgaber273@gmail.com>
2025-02-18 04:58:58 +0200

    feat(ui): add a landing page mock with some ui changes

---
+++ app/(dashboard)/dashboard/team/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "next/navigation";
+import { getTeamForUser, getUser } from "@/lib/db/queries";
+import { Settings } from "../settings";
+
+export default async function TeamPage() {
+  const user = await getUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
+  const teamData = await getTeamForUser(user.id);
+
+  if (!teamData) {
+    throw new Error("Team not found");
+  }
+
+  return <Settings teamData={teamData} />;
+}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { use, useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { CircleIcon, Home, LogOut } from 'lucide-react';
+import Link from "next/link";
+import { use, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { CircleIcon, Home, LogOut } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { useUser } from '@/lib/auth';
-import { signOut } from '@/app/(login)/actions';
-import { useRouter } from 'next/navigation';
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useUser } from "@/lib/auth";
+import { signOut } from "@/app/(login)/actions";
+import { useRouter } from "next/navigation";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -24,15 +24,24 @@ function Header() {
   async function handleSignOut() {
     await signOut();
     router.refresh();
-    router.push('/');
+    router.push("/");
   }
+
+  const getLogoRoute = () => {
+    if (user) {
+      return "/dashboard";
+    }
+    return "/";
+  };
 
   return (
     <header className="border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-        <Link href="/" className="flex items-center">
+        <Link href={getLogoRoute()} className="flex items-center">
           <CircleIcon className="h-6 w-6 text-orange-500" />
-          <span className="ml-2 text-xl font-semibold text-gray-900">ACME</span>
+          <span className="ml-2 text-xl font-semibold text-gray-900">
+            Lease Dynamix
+          </span>
         </Link>
         <div className="flex items-center space-x-4">
           <Link
@@ -44,26 +53,21 @@ function Header() {
           {user ? (
             <DropdownMenu open={isMenuOpen} onOpenChange={setIsMenuOpen}>
               <DropdownMenuTrigger>
-                <Avatar className="cursor-pointer size-9">
-                  <AvatarImage alt={user.name || ''} />
+                <Avatar className="cursor-pointer size-9 bg-orange-500">
+                  <AvatarImage alt={user.name || ""} />
                   <AvatarFallback>
-                    {user.email
-                      .split(' ')
+                    {(user.name || user.email)
+                      ?.split(" ")
                       .map((n) => n[0])
-                      .join('')}
+                      .join("")
+                      .toUpperCase()}
                   </AvatarFallback>
                 </Avatar>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="flex flex-col gap-1">
-                <DropdownMenuItem className="cursor-pointer">
-                  <Link href="/dashboard" className="flex w-full items-center">
-                    <Home className="mr-2 h-4 w-4" />
-                    <span>Dashboard</span>
-                  </Link>
-                </DropdownMenuItem>
                 <form action={handleSignOut} className="w-full">
                   <button type="submit" className="flex w-full">
-                    <DropdownMenuItem className="w-full flex-1 cursor-pointer">
+                    <DropdownMenuItem className="w-full flex-1 cursor-pointer hover:bg-red-500">
                       <LogOut className="mr-2 h-4 w-4" />
                       <span>Sign out</span>
                     </DropdownMenuItem>
